5abc87be7e69c4258b1cf1f79296e70a030043a2
Artem <mail@artempal.ru>
2016-11-21 21:57:51 +0300

    Прием данных из диалога

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "createdialog.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -9,6 +10,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     ui->label->hide();
     connect(ui->start_btn,SIGNAL(clicked(bool)), this, SLOT(open_createdialog()));
+
 }
 
 MainWindow::~MainWindow()
@@ -18,5 +20,17 @@ MainWindow::~MainWindow()
 void MainWindow::open_createdialog()
 {
     CreateDialog d;
+    connect(&d,SIGNAL(sendData(QString,QString,int)),this,SLOT(set_data(QString,QString,int)));
     d.exec();
 }
+
+void MainWindow::set_data(QString user_name, QString pet_name, int gender)
+{
+    _user_name = user_name;
+    _pet_name = pet_name;
+    _gender = gender;
+    ui->start_btn->hide();
+    ui->label->show();
+}
+
+
