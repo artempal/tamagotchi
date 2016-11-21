7dc2315ab9326af6654d37b0bdeba0c5969700a1
Artem <mail@artempal.ru>
2016-11-21 21:55:13 +0300

    функция отправки данных при нажатии на кнопку

--- createdialog.cpp
+++ createdialog.cpp
@@ -1,14 +1,32 @@
 #include "createdialog.h"
 #include "ui_createdialog.h"
+#include "mainwindow.h"
 
 CreateDialog::CreateDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::CreateDialog)
 {
     ui->setupUi(this);
+    connect(ui->ok_btn, SIGNAL(clicked()), this, SLOT(onSendData()));
 }
 
 CreateDialog::~CreateDialog()
 {
     delete ui;
 }
+
+void CreateDialog::onSendData()
+{
+    int gender;
+    if(ui->radioButton->isChecked())
+    {
+        gender = 1;
+    }
+    else
+    {
+        gender = 2;
+    }
+    emit sendData( ui->lineEdit->text(), ui->lineEdit_2->text() ,gender);
+    QDialog::close();
+}
+
